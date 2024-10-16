49cf6f757620a3ff8997999a98acf6f08d6c156d
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-10-16 23:07:11 +0530

    Time: 1 ms (77.96%) | Memory: 40.7 MB (76.87%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.java
+++ 1304-longest-happy-string/longest-happy-string.java
@@ -1,37 +1,29 @@
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
         StringBuilder sb = new StringBuilder();
-        
-        // Array to hold the counts of 'a', 'b', and 'c'
         int[][] counts = {{a, 'a'}, {b, 'b'}, {c, 'c'}};
 
         while (true) {
-            // Sort the array to always pick the character with the highest count
             Arrays.sort(counts, (x, y) -> y[0] - x[0]);
-
             boolean hasAdded = false;
 
-            // Try to add the most frequent character
             for (int i = 0; i < 3; i++) {
                 if (counts[i][0] == 0) {
-                    continue; // No more of this character left
+                    continue;
                 }
 
                 int len = sb.length();
-                // Check if adding the character will create three consecutive characters
                 if (len >= 2 && sb.charAt(len - 1) == counts[i][1] && sb.charAt(len - 2) == counts[i][1]) {
-                    continue; // Skip adding this character to avoid "aaa", "bbb", or "ccc"
+                    continue;
                 }
 
-                // Add the character
                 sb.append((char) counts[i][1]);
-                counts[i][0]--; // Decrease the count
+                counts[i][0]--;
                 hasAdded = true;
                 break;
             }
 
             if (!hasAdded) {
-                // If we couldn't add any character, break the loop
                 break;
             }
         }
