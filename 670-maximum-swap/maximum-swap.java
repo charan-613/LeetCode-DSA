b0babd22cbac8a136e0b76ddae5cdeafdd12dfa1
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-10-17 12:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (89.94%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,24 +1,26 @@
 class Solution {
     public int maximumSwap(int num) {
         char[] digits = Integer.toString(num).toCharArray();
-        int[] last = new int[10];
+        int maxIndex = digits.length - 1;
+        int x = -1, y = -1;
 
-        for (int i = 0; i < digits.length; i++) {
-            last[digits[i] - '0'] = i;
-        }
-
-        for (int i = 0; i < digits.length; i++) {
-            for (int d = 9; d > digits[i] - '0'; d--) {
-                if (last[d] > i) {
-                    char temp = digits[i];
-                    digits[i] = digits[last[d]];
-                    digits[last[d]] = temp;
-
-                    return Integer.parseInt(new String(digits));
-                }
+        for (int i = digits.length - 2; i >= 0; i--) {
+            if (digits[i] < digits[maxIndex]) {
+                x = i;
+                y = maxIndex;
+            } else if (digits[i] > digits[maxIndex]) {
+                maxIndex = i;
             }
         }
 
-        return num;
+        if (x == -1) {
+            return num;
+        }
+
+        char temp = digits[x];
+        digits[x] = digits[y];
+        digits[y] = temp;
+
+        return Integer.parseInt(new String(digits));
     }
 }
