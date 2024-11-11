3717dcd0d0fc9dc67b3e7d878284350c0fb9b819
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-11-12 00:33:58 +0530

    Time: 3 ms (65.79%) | Memory: 44.4 MB (70.20%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public boolean primeSubOperation(int[] nums) {
+        int prev = 0;
+        for (int n : nums) {
+            int upperBound = n - prev; // non-inclusive
+            // Find the largest prime less than the upper bound
+            int largestPrime = 0;
+            for (int i = upperBound - 1; i >= 2; i--) {
+                if (isPrime(i)) {
+                    largestPrime = i;
+                    break;
+                }
+            }
+            // If n - largestPrime <= prev, return false
+            if (n - largestPrime <= prev) {
+                return false;
+            }
+            prev = n - largestPrime;
+        }
+        return true;
+    }
+
+    // Helper function to check if a number is prime
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        
+        for (int f = 2; f <= Math.sqrt(n); f++) {
+            if (n % f == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
