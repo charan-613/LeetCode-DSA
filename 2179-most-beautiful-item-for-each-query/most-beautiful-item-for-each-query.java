ccaca0f0b85937ca0203c7a8e8367b336cdb8781
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-11-12 18:07:58 +0530

    Time: 55 ms (7.25%) | Memory: 81.7 MB (17.75%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        Arrays.sort(items, (a, b) -> Integer.compare(a[0], b[0]));
+        int[] result = new int[queries.length];
+        int[][] queryWithIndex = new int[queries.length][2];
+        
+        for (int i = 0; i < queries.length; i++) {
+            queryWithIndex[i][0] = queries[i];
+            queryWithIndex[i][1] = i;
+        }
+        Arrays.sort(queryWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int maxBeauty = 0;
+        int j = 0;
+
+        for (int[] query : queryWithIndex) {
+            int queryValue = query[0];
+            int queryIndex = query[1];
+
+            while (j < items.length && items[j][0] <= queryValue) {
+                maxBeauty = Math.max(maxBeauty, items[j][1]);
+                j++;
+            }
+            result[queryIndex] = maxBeauty;
+        }
+        return result;
+    }
+}
