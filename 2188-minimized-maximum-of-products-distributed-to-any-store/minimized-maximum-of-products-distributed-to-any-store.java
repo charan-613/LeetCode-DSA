ab587045aeb88e5a6adc3eb204dc72c3afb4f0dd
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-11-14 20:43:04 +0530

    Time: 23 ms (98.93%) | Memory: 58.1 MB (9.17%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,35 +1,34 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
-       int low = 1;
+        int low = 1;
         int high = max(quantities);
         int res = -1;
-        while (low <= high){
-            int mid = low + (high - low)/2;
-            if (canWork(quantities, mid, n)){
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canWork(quantities, mid, n)) {
                 res = mid;
                 high = mid - 1;
-            }
-            else{
+            } else {
                 low = mid + 1;
             }
         }
         return res;
     }
 
-    public static boolean canWork(int[] quantities, int item, int total_stores){
+    public static boolean canWork(int[] quantities, int item, int total_stores) {
         int need_stores = 0;
 
-        for(int quantity : quantities){
+        for (int quantity : quantities) {
             need_stores += (quantity - 1) / item + 1;
-            if(need_stores > total_stores){
+            if (need_stores > total_stores) {
                 return false;
             }
         }
         return true;
     }
 
-    public static int max(int[] arr){
-        int max = arr[0]; 
+    public static int max(int[] arr) {
+        int max = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
