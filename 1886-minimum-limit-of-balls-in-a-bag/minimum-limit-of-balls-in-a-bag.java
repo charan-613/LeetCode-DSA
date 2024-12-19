68970d06d29ce9939431bd7606a8a526650f7bd1
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-12-19 16:27:24 +0530

    Time: 49 ms (35.44%) | Memory: 59.8 MB (95.54%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -1,15 +1,45 @@
 class Solution {
-    public int minimumSize(int[] nums, int maxOps) {
-        int low = 1, high = Arrays.stream(nums).max().getAsInt();
-        while (low < high) {
-            int mid = (low + high) / 2;
-            int ops = 0;
-            for (int n : nums) {
-                ops += (n - 1) / mid;
+    public int minimumSize(int[] nums, int maxOperations) {
+        int l = 1;
+        int r =  (int) Math.pow(10, 9);
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(isPossible(nums, maxOperations, mid)){
+                r = mid - 1;
             }
-            if (ops <= maxOps) high = mid;
-            else low = mid + 1;
+            else{
+                l = mid + 1;
+            }
+        }  
+        return l;
+    }
+
+    public static boolean isPossible(int[] nums, int maxOperations, int k){
+        for(int i = 0; i < nums.length; i++){
+            int val = nums[i];
+
+            int temp = val / k;
+            if(val % k != 0){
+                temp++;
+            }
+            temp--;
+
+            maxOperations -= temp;
+
+            if(maxOperations < 0){
+                return false;
+            }
         }
-        return high;
+        return true;
+    }
+
+    public static int getMax(int[] nums){
+        int max = Integer.MIN_VALUE;
+        for(int num : nums){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
     }
 }
