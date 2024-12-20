015a7be3e42753b3dbd182a3524e511e0b32d807
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-12-20 15:02:18 +0530

    Time: 10 ms (34.32%) | Memory: 49 MB (32.66%) - LeetSync

---
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean isOddLevel = false;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<TreeNode> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                currentLevel.add(node);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            if (isOddLevel) {
+                int left = 0, right = currentLevel.size() - 1;
+                while (left < right) {
+                    int temp = currentLevel.get(left).val;
+                    currentLevel.get(left).val = currentLevel.get(right).val;
+                    currentLevel.get(right).val = temp;
+                    left++;
+                    right--;
+                }
+            }
+
+            isOddLevel = !isOddLevel;
+        }
+
+        return root;
+    }
+}
