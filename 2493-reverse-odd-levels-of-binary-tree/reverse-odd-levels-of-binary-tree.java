e7799bc491766a2d243d59c6855d9beb1760d3b7
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2024-12-20 15:03:01 +0530

    Time: 1 ms (72.88%) | Memory: 49.1 MB (30.63%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,58 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        boolean isOddLevel = false;
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<TreeNode> currentLevel = new ArrayList<>();
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.poll();
-                currentLevel.add(node);
-
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-
-            if (isOddLevel) {
-                int left = 0, right = currentLevel.size() - 1;
-                while (left < right) {
-                    int temp = currentLevel.get(left).val;
-                    currentLevel.get(left).val = currentLevel.get(right).val;
-                    currentLevel.get(right).val = temp;
-                    left++;
-                    right--;
-                }
-            }
-
-            isOddLevel = !isOddLevel;
-        }
-
+        dfs(root.left, root.right, 0);
         return root;
+    }
+    
+    public void dfs(TreeNode left, TreeNode right, int depth) {
+        if (left == null && right == null) return;
+
+        if (depth % 2 == 0) {
+            int temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+
+        dfs(left.left, right.right, depth + 1);
+        dfs(left.right, right.left, depth + 1);
     }
 }
