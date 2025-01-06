e4a9d999ad0972da3b87a271c9deae7c7409daf5
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2025-01-06 18:56:33 +0530

    Time: 3 ms (94.79%) | Memory: 45.1 MB (14.93%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,23 +1,30 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
-        int[] answer = new int[n];
+        int[] result = new int[n];
+        int leftBalls = 0, rightBalls = 0, leftMoves = 0, rightMoves = 0;
 
-        int count = 0;
-        int operations = 0;
-        for(int i  = 0; i < n; i++){
-            answer[i] += operations;
-            count += boxes.charAt(i) - '0';
-            operations += count;
+        // Initial pass to calculate the number of balls on the right side
+        for (int i = 0; i < n; i++) {
+            if (boxes.charAt(i) == '1') {
+                rightBalls++;
+                rightMoves += i;
+            }
         }
 
-        count = 0;
-        operations = 0;
-        for(int i = n - 1; i >= 0; i--){
-            answer[i] += operations;
-            count += boxes.charAt(i) - '0';
-            operations += count;
+        // Second pass to calculate the operations needed for each box
+        for (int i = 0; i < n; i++) {
+            result[i] = leftMoves + rightMoves;
+
+            if (boxes.charAt(i) == '1') {
+                leftBalls++;
+                rightBalls--;
+            }
+
+            leftMoves += leftBalls;
+            rightMoves -= rightBalls;
         }
-        return answer;
+
+        return result;
     }
 }
