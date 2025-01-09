662e8b1e6f3430fec95e8f1e8a14513d609d8ab4
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2025-01-09 16:02:05 +0530

    Time: 837 ms (29.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT a1.player_id, a1.event_date AS first_login
+FROM Activity a1
+LEFT JOIN Activity a2
+  ON a1.player_id = a2.player_id
+  AND a1.event_date > a2.event_date
+WHERE a2.player_id IS NULL;
