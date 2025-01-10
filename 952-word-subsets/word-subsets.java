da0f7bad59c3f30466eff3a65581a5f0971e777e
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2025-01-10 17:06:34 +0530

    Time: 12 ms (92.44%) | Memory: 54.7 MB (64.53%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,38 +1,30 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        List<String> result = new ArrayList<>();
+        int[] req = new int[26];
+        for (String word : words2) {
+            int[] cur = new int[26];
+            for (char c : word.toCharArray()) {
+                cur[c - 'a']++;
+                req[c - 'a'] = Math.max(req[c - 'a'], cur[c - 'a']);
+            }
+        }
         
-        int[] maxFreq = new int[26];
-        for(String word : words2){
-            int[] freq = getFrequency(word);
-            for(int i  = 0; i < 26; i++){
-                maxFreq[i] = Math.max(maxFreq[i], freq[i]);
+        List<String> ans = new ArrayList<>();
+        for (String word : words1) {
+            int[] cur = new int[26];
+            for (char c : word.toCharArray()) cur[c - 'a']++;
+            
+            boolean isValid = true;
+            for (int i = 0; i < 26; i++) {
+                if (cur[i] < req[i]) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid){
+                ans.add(word);
             }
         }
-
-        for(String word : words1){
-            int[] freq = getFrequency(word);
-            if(isUniversal(freq, maxFreq)){
-                result.add(word);
-            }
-        }
-        return result;
-    }
-    
-    public int[] getFrequency(String word){
-        int[] freq = new int[26];
-        for(char c : word.toCharArray()){
-            freq[c - 'a']++;
-        }
-        return freq;
-    }
-
-    public boolean isUniversal(int[] freq, int[] maxFreq){
-        for(int i = 0; i < 26; i++){
-            if(freq[i] < maxFreq[i]){
-                return false;
-            }
-        }
-        return true;
+        return ans;
     }
 }
