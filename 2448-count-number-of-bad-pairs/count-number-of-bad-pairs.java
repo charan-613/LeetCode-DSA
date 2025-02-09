da58b863df99f3d05a66bf330f06cb251fe47021
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2025-02-09 16:35:28 +0530

    Time: 48 ms (12.94%) | Memory: 59.3 MB (34.41%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
@@ -1,14 +1,14 @@
 class Solution {
     public long countBadPairs(int[] nums) {
         int n = nums.length;
-        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> hm = new HashMap<>();
         long goodPairs = 0;
         
         for (int i = 0; i < n; i++) {
-            if (freq.containsKey(nums[i] - i)) {
-                goodPairs += freq.get(nums[i] - i);
+            if (hm.containsKey(nums[i] - i)) {
+                goodPairs += hm.get(nums[i] - i);
             }
-            freq.put(nums[i] - i, freq.getOrDefault(nums[i] - i, 0) + 1);
+            hm.put(nums[i] - i, hm.getOrDefault(nums[i] - i, 0) + 1);
         }
         
         long badPairs = (long) n * (n - 1) / 2 - goodPairs;
