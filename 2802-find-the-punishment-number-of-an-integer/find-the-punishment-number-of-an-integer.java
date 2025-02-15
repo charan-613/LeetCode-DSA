b754f2c6c09d11547ddcf154f89db83c1e5ea3d4
Charan_Sankeerth <92849706+charan-613@users.noreply.github.com>
2025-02-15 16:30:20 +0530

    Time: 162 ms (23.20%) | Memory: 44.5 MB (54.23%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int punishmentNumber(int n) {
+        int punishmentNum = 0;
+
+        for (int currentNum = 1; currentNum <= n; currentNum++) {
+            int squareNum = currentNum * currentNum;
+
+            if (canPartition(Integer.toString(squareNum), currentNum)) {
+                punishmentNum += squareNum;
+            }
+        }
+        return punishmentNum;
+    }
+
+    public boolean canPartition(String stringNum, int target) {
+        if (stringNum.isEmpty() && target == 0) {
+            return true;
+        }
+        if (target < 0) {
+            return false;
+        }
+
+        for (int index = 0; index < stringNum.length(); index++) {
+            String left = stringNum.substring(0, index + 1);
+            String right = stringNum.substring(index + 1);
+            int leftNum = Integer.parseInt(left);
+
+            if (canPartition(right, target - leftNum)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
